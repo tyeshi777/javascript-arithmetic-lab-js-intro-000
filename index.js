c2c533fec89a8435467efa228f7e2429465a7ca8
tenzing yeshi <tyeshi777@yahoo.com>
2018-11-01 20:01:22 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+var a, b;
+
+function add(){
+  a + b;
+}
+function substract(){
+  a - b;
+}
+function multiply(){
+  a * b;
+}
+function divide(){
+  a / b;
+}
+add(1, 80);
+substract(60, 40);
+multiply(2, 3.4);
+divide(5.0, 2.5);
